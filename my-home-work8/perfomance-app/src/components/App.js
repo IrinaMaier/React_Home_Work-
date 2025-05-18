f506def8096297f0636c7c1f998f00c4ee7010fa
Maier Irina <maieririna@mail.de>
2025-05-18 22:14:59 +0200

    added my home work number8

---
+++ my-home-work8/perfomance-app/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListItem from './ListItem';
+
+function App() {
+  return (
+    <div className="App">
+   <ListItem/>
+    </div>
+  );
+}
+
+export default App;
