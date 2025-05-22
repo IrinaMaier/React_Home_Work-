ec1084f5a792cb042fbf4a8df8cff31938ab331d
Maier Irina <maieririna@mail.de>
2025-05-22 22:43:36 +0200

    Add my home work with thema dynamic form

---
+++ my-home-work9/dynamic-form/src/components/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import DynamicForm from "../components/DynamicForm";
+
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Dynamic Form</h1>
+      <DynamicForm/>
+    </div>
+  );
+}
+
+export default App;
